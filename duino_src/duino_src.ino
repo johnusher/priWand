879fda095bcd4f2b6e5791b5947885e86efdf6af
John Usher <yesjhon@gmail.com>
2021-11-12 13:33:44 +0100

    mode 0: sold, mode 1: flash

--- duino_src/duino_src.ino
+++ duino_src/duino_src.ino
@@ -20,7 +20,7 @@ void setup() {
   // Serial.begin(19200);
   Serial.begin(115200);
 
-  idleCol =  strip.Color(idleColR, idleColG, idleColB);
+  idleCol =  strip.Color(idleColG, idleColR, idleColB);
 }
 
 /**
@@ -76,12 +76,12 @@ void loop() {
 
 void checkSerialInput() {
 
-
+  // expect a strong like 0,1,123,456,789*
 
   //  https://forum.arduino.cc/t/split-string-by-delimiters/373124/3
 
-//  or here:
-//  https://forum.arduino.cc/t/splitting-up-a-string-with-a-delimiter-and-storing-each-cut-in-an-array/628574
+  //  or here:
+  //  https://forum.arduino.cc/t/splitting-up-a-string-with-a-delimiter-and-storing-each-cut-in-an-array/628574
 
   // input message is delimited with semi colon and termniated with newline
   // must begin with a 0
@@ -90,107 +90,189 @@ void checkSerialInput() {
   //  strip.clear();
   serial_in = Serial.read();
 
+
   if (serial_in == -1) {
     //    empty
     return;
 
   }
 
-  // read in semi-colon delimited message
-  serialResponse  = Serial.readStringUntil('\r\n');
-
+  serialResponse = Serial.readStringUntil('\r\n');
+  //  Serial.println(serialResponse);
 
   // Convert from String Object to String.
   char buf[sizeof(sz)];
   serialResponse.toCharArray(buf, sizeof(buf));
   char *p = buf;
   char *str;
-  i=0;
-  while ((str = strtok_r(p, ";", &p)) != NULL){ // delimiter is the semicolon
+  i = 1;
+  while ((str = strtok_r(p, ";", &p)) != NULL) { // delimiter is the semicolon
     Serial.println(str);
-    buf[i] = str;
-    i=i+1;
+    strings[i] = str;
+    i = i + 1;
   }
-  i=i-1;
+  i = i - 1;
+
+  //  Serial.println("i:");
+  //  Serial.println(i);
+
+  for (j = 1; j < i + 1; j++) {
+    //      Serial.println("j:");
+    data[j] = String(strings[j]).toInt();
+    //    Serial.print("a:");
+    //    Serial.println(strings[j] );
+    //    Serial.print("b:");
+    //    Serial.println(data[j] );
+    //    Serial.println();
+  }
 
   if (serial_in == '0') {
-
-    // code 0 = solid
-//    Serial.println(serial_in);
-    idleColR = buf[0];
-    idleColG = buf[1];
-    idleColB = buf[2];
-    Serial.println("colours");
-    Serial.println(buf[0]);
-    Serial.println(idleColG);
-    Serial.println(idleColB);
-    
+    mode0();
   }
+
+   if (serial_in == '1') {
+    mode1();
+  }
+
+  return;
+
+}
+
+void mode0() {
+  Serial.println("mode 0");
+  // solid
+  while (Serial.available() == 0) {
+
+    idleC = 0;
+
+    idleColR = data[1];
+    idleColG = data[2];
+    idleColB = data[3];
+
+//    Serial.print("idleColG:");
+//    Serial.println(idleColG);
+//     Serial.print("idleColR:");
+//    Serial.println(idleColR);
+//     Serial.print("idleColB:");
+//    Serial.println(idleColB);
+
+    strip.setPixelColor(0, idleColG, idleColR, idleColB);
+    strip.show();
+
+    for (i = 0; i < 65530; i++) {
+      checkSerialInput();
+    }
+
+  }
+
+}
+
+void mode1() {
+  Serial.println("mode 2");
+  // flash
+  while (Serial.available() == 0) {
+
+    idleC = 0;
+
+    idleColR = data[1];
+    idleColG = data[2];
+    idleColB = data[3];
+    flashRate = data[4];
+
+//    Serial.print("idleColG:");
+//    Serial.println(idleColG);
+//     Serial.print("idleColR:");
+//    Serial.println(idleColR);
+//     Serial.print("idleColB:");
+//    Serial.println(idleColB);
+
+    strip.setPixelColor(0, idleColG, idleColR, idleColB);
+    strip.show();
+
+    for (i = 0; i < flashRate; i++) {
+      checkSerialInput();
+    }
+
+    strip.setPixelColor(0, 0, 0, 0);
+    strip.show();
+
+    for (i = 0; i < flashRate; i++) {
+      checkSerialInput();
+    }
+
+  }
+
+}
+
+
+//  if (serial_in == '0') {
+//
+//    // code 0 = solid
+//    //    Serial.println(serial_in);
+//    idleColR = buf[0];
+//    idleColG = buf[1];
+//    idleColB = buf[2];
+//    Serial.println("colours");
+//    Serial.println(buf[0]);
+//    Serial.println(idleColG);
+//    Serial.println(idleColB);
+//
+//  }
 
 //  mode 1= solid flash
 //  colour A
 //  flash interval
 
 
-  //  messSize = strlen(serial_in);
+//  messSize = strlen(serial_in);
 
-  //  Serial.println(messSize);
+//  Serial.println(messSize);
 
-  // say what you got:
-  if (messSize > 2) {
 
-    //     Serial.println(messSize);
-    ////    Serial.println(serial_in);
-    ////    Serial.println(messSize);
-    //
-    ////    Serial.flush();
-
-  }
-
-  //  if (serial_in == '0') {
-  //    SMode = 0;
-  //    Serial.println("mode 0");
-  //    Serial.flush();
-  //    Serial.println("a");
-  //
-  //    idleC = 128;
-  //    idleColR = 0;
-  //    idleColG = 0;
-  //    idleColB = 128;
-  //
-  //    strip.setPixelColor(idleC, idleColR, idleColG, idleColB);  // show blue light moving along
-  //    Serial.println("b");
-  //
-  //
-  //    breakFlag = 0;
-  //    idleC = 0;
-  //
-  //  }
-  //
-  //  else if (serial_in == '1') {
-  //    SMode = 1;
-  //    Serial.println("mode 1");
-  //    Serial.flush();
-  //
-  //    colorWipe(strip.Color(0, 0 , maxC ), 25); // g
-  //    Serial.print("breakFlag = ");
-  //    Serial.println(breakFlag);
-  //
-  //
-  //    if (breakFlag == 1) {
-  //      colorWipe(strip.Color(0, 0 , 0), 50); // off
-  //    }
-  //    else {
-  //      wipeReverse = !wipeReverse;
-  //    }
-  //    breakFlag = 0;
-  //    idleC = 0;
-  //
-  //  }
+//  if (serial_in == '0') {
+//    SMode = 0;
+//    Serial.println("mode 0");
+//    Serial.flush();
+//    Serial.println("a");
+//
+//    idleC = 128;
+//    idleColR = 0;
+//    idleColG = 0;
+//    idleColB = 128;
+//
+//    strip.setPixelColor(idleC, idleColR, idleColG, idleColB);  // show blue light moving along
+//    Serial.println("b");
+//
+//
+//    breakFlag = 0;
+//    idleC = 0;
+//
+//  }
+//
+//  else if (serial_in == '1') {
+//    SMode = 1;
+//    Serial.println("mode 1");
+//    Serial.flush();
+//
+//    colorWipe(strip.Color(0, 0 , maxC ), 25); // g
+//    Serial.print("breakFlag = ");
+//    Serial.println(breakFlag);
+//
+//
+//    if (breakFlag == 1) {
+//      colorWipe(strip.Color(0, 0 , 0), 50); // off
+//    }
+//    else {
+//      wipeReverse = !wipeReverse;
+//    }
+//    breakFlag = 0;
+//    idleC = 0;
+//
+//  }
 
 
 
-}
+
 
 void myStripShow() {
   if (Serial.available() > 0) {
@@ -241,36 +323,4 @@ void colorWipe(uint32_t c, uint8_t wait) {
   //  noTone(buzzer);     // Stop sound...
 
 
-}
-
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
-    for (i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-
-      //     tone(buzzer, i + j); // Send xm sound signal...
-
-    }
-    myStripShow();
-    delay(wait);
-  }
-
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if (WheelPos < 85) {
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-    WheelPos -= 170;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
 }
